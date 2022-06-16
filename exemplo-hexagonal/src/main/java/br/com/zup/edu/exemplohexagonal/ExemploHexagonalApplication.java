3572ca3461e290628cf17e1d079d2236ecf4ac4d
Adriano Avelino <adriano-st777@hotmail.com>
2022-06-15 21:27:07 -0300

    adiciona projeto exemplo inicial

---
+++ exemplo-hexagonal/src/main/java/br/com/zup/edu/exemplohexagonal/ExemploHexagonalApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.exemplohexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploHexagonalApplication.class, args);
+	}
+
+}
---
+++ exemplo-hexagonal/src/test/java/br/com/zup/edu/exemplohexagonal/ExemploHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.exemplohexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
