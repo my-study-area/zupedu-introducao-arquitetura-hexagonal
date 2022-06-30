7f70f193588ae450d3fa720cab7a9039c94f6a85
Adriano Avelino <adriano-st777@hotmail.com>
2022-06-29 21:21:58 -0300

    atualiza parâmetro do método para utilizar um interface (porta)

--- bancodigital/src/main/java/com/zupedu/bancodigital/cadastrarProposta/PropostaController.java
+++ bancodigital/src/main/java/com/zupedu/bancodigital/cadastrarProposta/PropostaController.java
@@ -19,7 +19,7 @@ public class PropostaController {
     }
 
     @PostMapping
-    public ResponseEntity<?> cadastrar(@RequestBody @Valid NovaPropostaRequestAdapter novaPropostaRequest,
+    public ResponseEntity<?> cadastrar(@RequestBody @Valid NovaPropostaInputPort novaPropostaRequest,
                                        UriComponentsBuilder uriBuilder) {
         var proposta = novaPropostaRequest.toModel();
         service.cadastrar(proposta);
