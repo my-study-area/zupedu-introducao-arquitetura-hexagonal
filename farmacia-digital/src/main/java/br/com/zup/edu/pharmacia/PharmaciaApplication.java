095e43a777cd7900123698e8b00919b00125cc67
Adriano Avelino <adriano-st777@hotmail.com>
2022-06-30 21:27:59 -0300

    adiciona a configuração inicial do spring

--- farmacia-digital/src/main/java/br/com/zup/edu/pharmacia/PharmaciaApplication.java
+++ farmacia-digital/src/main/java/br/com/zup/edu/pharmacia/PharmaciaApplication.java
@@ -1,4 +1,11 @@
 package br.com.zup.edu.pharmacia;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PharmaciaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaciaApplication.class, args);
+    }
 }
