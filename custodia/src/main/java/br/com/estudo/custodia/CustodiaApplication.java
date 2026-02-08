e54cb3953e53dd9d1c2eff4da0fab7c04d0a1d33
Adriano Avelino <adriano-st777@hotmail.com>
2026-02-07 21:13:50 -0300

    adiciona projeto custodia

---
+++ custodia/src/main/java/br/com/estudo/custodia/CustodiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.custodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustodiaApplication.class, args);
+	}
+
+}
