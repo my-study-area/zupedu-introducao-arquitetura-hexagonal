cdb3e8ce8b6973c2439d1355cc1d0a31b6a9f4cd
Adriano Avelino <adriano-st777@hotmail.com>
2026-02-08 18:51:26 -0300

    move mock do serviço de liquidação para pacote

--- custodia/src/main/java/br/com/estudo/custodia/mock/CustodiaController.java
+++ custodia/src/main/java/br/com/estudo/custodia/mock/CustodiaController.java
@@ -3,23 +3,15 @@ package br.com.estudo.custodia.mock;
 import br.com.estudo.custodia.LiquidacaoRequestDTO;
 import br.com.estudo.custodia.LiquidacaoResponseDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class CustodiaController {
 
-    @GetMapping("/v1/baixar")
-    public ResponseEntity<Object> baixar() {
-        return ResponseEntity.ok(Map.of("status", "200"));
-    }
-
     @PostMapping("/v1/baixar")
-    public ResponseEntity<LiquidacaoResponseDTO> baixar2(@RequestBody LiquidacaoRequestDTO request) {
+    public ResponseEntity<LiquidacaoResponseDTO> baixar(@RequestBody LiquidacaoRequestDTO request) {
         LiquidacaoResponseDTO response = new LiquidacaoResponseDTO(200, request.numeroContrato());
         return ResponseEntity.ok(response);
     }
